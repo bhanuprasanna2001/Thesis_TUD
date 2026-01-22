c1358315b87e59baf64bfc55254bd054b11e1171
Bhanu Prasanna <bhanu.prasanna2001@gmail.com>
2026-01-22 20:01:50 +0100

    Remove CityscapesDataModule and update exports in data module

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,4 @@
-from .datasets import get_mnist_loaders
 from .mnist import MNISTDataModule
 from .oxford_pet import OxfordPetDataModule
-from .cityscapes import CityscapesDataModule
 
-__all__ = ["get_mnist_loaders", "MNISTDataModule", "OxfordPetDataModule", "CityscapesDataModule"]
+__all__ = ["MNISTDataModule", "OxfordPetDataModule"]
